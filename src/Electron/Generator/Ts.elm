7e9a02298d94710eb7b7c73b271e2bc37fca3a40
Dillon Kearns <dillon@dillonkearns.com>
2017-07-26 19:23:40 -0400

    Add hardcoded union generator.

--- src/Electron/Generator/Ts.elm
+++ src/Electron/Generator/Ts.elm
@@ -6,3 +6,8 @@ import Electron.Ipc
 generateInterface : Electron.Ipc.ElmIpc -> String
 generateInterface something =
     "interface HideWindow {\n  message: 'HideWindow'\n}"
+
+
+generateUnion : List Electron.Ipc.ElmIpc -> String
+generateUnion elmIpcIpcElectronList =
+    "type ElmIpc =\n | ShowFeedbackForm"
--- tests/TsInterfaceTests.elm
+++ tests/TsInterfaceTests.elm
@@ -8,11 +8,17 @@ import Test exposing (Test, describe, test)
 
 suite : Test
 suite =
-    describe "ts interface"
-        [ test "with no parameters" <|
+    describe "ts"
+        [ test "interface for msg with no parameters" <|
             \() ->
                 Electron.Ipc.Msg "HideWindow"
                     |> Electron.Generator.Ts.generateInterface
                     |> Expect.equal
                         "interface HideWindow {\n  message: 'HideWindow'\n}"
+        , test "union" <|
+            \() ->
+                [ Electron.Ipc.Msg "HideWindow" ]
+                    |> Electron.Generator.Ts.generateUnion
+                    |> Expect.equal
+                        "type ElmIpc =\n | ShowFeedbackForm"
         ]
