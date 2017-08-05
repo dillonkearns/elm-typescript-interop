0561f476e7bccd8efa7d8185bb6a7caf85e74988
Dillon Kearns <dillon@dillonkearns.com>
2017-08-05 13:08:43 -0400

    Extract e2e helpers into separate file.

--- tests/e2e/e2e.ts
+++ tests/e2e/e2e.ts
@@ -1,89 +1,16 @@
 const approvals = require('approvals')
 const { describe, it } = require('mocha')
-import * as child_process from 'child_process'
-import * as fs from 'fs'
 import * as path from 'path'
-import { assert } from 'chai'
+import {
+  KaleidoscopeReporter,
+  assertCommandOutput,
+  expectCommandSuccess,
+  approveFile
+} from './helpers'
 
-class KaleidoscopeReporter {
-  public name: string = 'ksdiff'
-  canReportOn(receivedFilePath: string) {
-    return true
-  }
-  report(approvedFilePath: string, receivedFilePath: string) {
-    let commandOutput = child_process.execSync(
-      `ksdiff ${approvedFilePath} ${receivedFilePath}`
-    )
-  }
-}
 approvals.configure({
   reporters: [new KaleidoscopeReporter()]
 })
-
-interface ExpectedStdOut {
-  type: 'stdout'
-  expected: string
-}
-
-interface ExpectedStdErr {
-  type: 'stderr'
-  expected: string
-}
-
-interface SuccessResult {
-  type: 'success-result'
-  stdout: string
-}
-
-interface ErrorResult {
-  type: 'error-result'
-  stderr: string
-  stdout: string
-  status: number
-}
-const approveFile = (approvalDescription: string, relativePath: string) => {
-  const fileBuffer = fs.readFileSync(path.join(relativePath))
-  approvals.verify(__dirname, approvalDescription, fileBuffer.toString())
-}
-
-type ExpectedOutput = ExpectedStdOut | ExpectedStdErr
-type CommandResult = SuccessResult | ErrorResult
-
-const assertCommandOutput = (command: string, approvalName: string) => {
-  let commandResult: CommandResult
-  try {
-    let commandOutput = child_process.execSync(command).toString()
-    commandResult = { type: 'success-result', stdout: commandOutput }
-  } catch (error) {
-    const { status, stdout, stderr } = error
-    commandResult = {
-      type: 'error-result',
-      stderr: stderr && stderr.toString(),
-      stdout: stdout && stdout.toString(),
-      status: status
-    }
-  }
-  approvals.verifyAsJSON(__dirname, approvalName, commandResult)
-}
-
-const expectCommandSuccess = (command: string) => {
-  try {
-    let commandOutput = child_process.execSync(command).toString()
-  } catch (error) {
-    const { status, stdout, stderr } = error
-    let result = {
-      type: 'error-result',
-      stderr: stderr && stderr.toString(),
-      stdout: stdout && stdout.toString(),
-      status: status
-    }
-    assert.equal(
-      status,
-      0,
-      `Command '${command} failed unexpectedly: ${JSON.stringify(result)}'`
-    )
-  }
-}
 
 describe('end to end', function() {
   it('gives error for non-existent input file', () => {
@@ -127,6 +54,5 @@ describe('end to end', function() {
     expectCommandSuccess(command)
     approveFile('validInputTs', outputTsPath)
     approveFile('validInputElm', outputElmPath)
-    let output = child_process.execSync(command)
   }).timeout(30000)
 })
