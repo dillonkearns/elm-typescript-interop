d6bb7327121855d1b6245b708b638427e63ff4a5
Dillon Kearns <dillon@dillonkearns.com>
2018-09-01 19:09:49 -0700

    Simplify an end to end test case.

--- test_data/unqualifiedAliasImport/Main.elm
+++ test_data/unqualifiedAliasImport/Main.elm
@@ -1,14 +1,6 @@
-port module Main exposing (Flags, Model, emptyIncomingMessage, generatedFiles, getMaybe, inbound, incomingJsonValue, init, main, outgoingArray, outgoingBoolAlias, outgoingJsonValues, outgoingList, outgoingRecord, parsingError, sendTuple, update)
+port module Main exposing (main)
 
 import Aliases exposing (AliasForBool)
-import Array
-import Json.Decode as Decode
-import Json.Encode
-
-
-
--- it shouldn't fail because there are line-comments
-{- or block comments -}
 
 
 type alias Model =
@@ -21,7 +13,6 @@ type alias Flags =
 
 init : Flags -> ( Model, Cmd msg )
 init flags =
-    -- this shouldn't cause parsing to fail!
     () ! []
 
 
@@ -37,39 +28,6 @@ main =
         , update = update
         , subscriptions = \_ -> Sub.none
         }
-
-
-port generatedFiles : String -> Cmd msg
-
-
-port parsingError : String -> Cmd msg
-
-
-port inbound : (Int -> msg) -> Sub msg
-
-
-port getMaybe : (Maybe Bool -> thisIsAMsg) -> Sub thisIsAMsg
-
-
-port sendTuple : Maybe ( String, Int, Int ) -> Cmd msg
-
-
-port outgoingList : List ( Int, Int ) -> Cmd msg
-
-
-port outgoingArray : Array.Array String -> Cmd msg
-
-
-port outgoingRecord : { id : Int, username : String, avatarUrl : String } -> Cmd msg
-
-
-port outgoingJsonValues : List Json.Encode.Value -> Cmd msg
-
-
-port incomingJsonValue : (Decode.Value -> msg) -> Sub msg
-
-
-port emptyIncomingMessage : (() -> msg) -> Sub msg
 
 
 port outgoingBoolAlias : AliasForBool -> Cmd msg
