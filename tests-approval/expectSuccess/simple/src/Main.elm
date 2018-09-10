ccec9f548dd9f7bf3c09e1dad1c757e6c345a9ef
Dillon Kearns <dillon@dillonkearns.com>
2018-09-10 15:15:16 -0700

    Move approval tests folder.

---
+++ tests-approval/expectSuccess/simple/src/Main.elm
@@ -0,0 +1,72 @@
+port module Main exposing (AliasForBool, Flags, Model, emptyIncomingMessage, generatedFiles, getMaybe, inbound, incomingJsonValue, init, main, outgoingArray, outgoingBoolAlias, outgoingJsonValues, outgoingList, outgoingRecord, parsingError, sendTuple, update)
+
+import Array
+import Json.Decode as Decode
+import Json.Encode
+
+
+type alias Model =
+    ()
+
+
+type alias AliasForBool =
+    Bool
+
+
+type alias Flags =
+    { elmModuleFileContents : String }
+
+
+init : Flags -> ( Model, Cmd msg )
+init flags =
+    () ! []
+
+
+update : msg -> Model -> ( Model, Cmd msg )
+update msg model =
+    model ! []
+
+
+main : Program Flags Model msg
+main =
+    Platform.programWithFlags
+        { init = init
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
+
+
+port generatedFiles : String -> Cmd msg
+
+
+port parsingError : String -> Cmd msg
+
+
+port inbound : (Int -> msg) -> Sub msg
+
+
+port getMaybe : (Maybe Bool -> thisIsAMsg) -> Sub thisIsAMsg
+
+
+port sendTuple : Maybe ( String, Int, Int ) -> Cmd msg
+
+
+port outgoingList : List ( Int, Int ) -> Cmd msg
+
+
+port outgoingArray : Array.Array String -> Cmd msg
+
+
+port outgoingRecord : { id : Int, username : String, avatarUrl : String } -> Cmd msg
+
+
+port outgoingJsonValues : List Json.Encode.Value -> Cmd msg
+
+
+port incomingJsonValue : (Decode.Value -> msg) -> Sub msg
+
+
+port emptyIncomingMessage : (() -> msg) -> Sub msg
+
+
+port outgoingBoolAlias : AliasForBool -> Cmd msg
---
+++ tests-approval/expectSuccess/validSyntax/src/Main/index.d.ts
@@ -0,0 +1,52 @@
+// WARNING: Do not manually modify this file. It was generated using:
+// https://github.com/dillonkearns/elm-typescript-interop
+// Type definitions for Elm ports
+export as namespace Elm
+
+
+export interface App {
+  ports: {
+    generatedFiles: {
+      subscribe(callback: (data: string) => void): void
+    }
+    parsingError: {
+      subscribe(callback: (data: string) => void): void
+    }
+    inbound: {
+      send(data: number): void
+    }
+    getMaybe: {
+      send(data: boolean | null): void
+    }
+    sendTuple: {
+      subscribe(callback: (data: [string, number, number] | null) => void): void
+    }
+    outgoingList: {
+      subscribe(callback: (data: [number, number][]) => void): void
+    }
+    outgoingArray: {
+      subscribe(callback: (data: string[]) => void): void
+    }
+    outgoingRecord: {
+      subscribe(callback: (data: { id: number; username: string; avatarUrl: string }) => void): void
+    }
+    outgoingJsonValues: {
+      subscribe(callback: (data: any[]) => void): void
+    }
+    incomingJsonValue: {
+      send(data: any): void
+    }
+    emptyIncomingMessage: {
+      send(data: null): void
+    }
+    outgoingBoolAlias: {
+      subscribe(callback: (data: boolean) => void): void
+    }
+  }
+}
+    
+
+export namespace Main {
+  export function fullscreen(flags: { elmModuleFileContents: string }): App
+  export function embed(node: HTMLElement | null, flags: { elmModuleFileContents: string }): App
+}
