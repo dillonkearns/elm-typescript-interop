f9632ac6cc7df250c8454ee34d47176eefcd9838
Dillon Kearns <dillon@dillonkearns.com>
2018-09-11 16:14:29 -0700

    Update index.d.ts format manually.

--- tests-approval/expectSuccess/elm19Simple/src/Main/index.d.ts
+++ tests-approval/expectSuccess/elm19Simple/src/Main/index.d.ts
@@ -1,16 +1,16 @@
 // WARNING: Do not manually modify this file. It was generated using:
 // https://github.com/dillonkearns/elm-typescript-interop
 // Type definitions for Elm ports
-export as namespace Elm
 
+export namespace Elm {
+  namespace Main {
+    export interface App {
+      ports: {};
+    }
 
-export interface App {
-  ports: {
-
+    export function init(options: {
+      node?: HTMLElement | null;
+      flags: null;
+    }): Elm.Main.App;
   }
-}
-    
-
-export namespace Main {
-  export function init(options: { node: HTMLElement; flags: null }): App
 }
