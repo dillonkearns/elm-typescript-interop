0a5ccaf6e2471a94ea69ce35e8ce7856b67bc073
Dillon Kearns <dillon@dillonkearns.com>
2018-09-11 06:14:32 -0700

    Parse elm-version in elm project config.

--- src/ElmProjectConfig.elm
+++ src/ElmProjectConfig.elm
@@ -1,6 +1,7 @@
 module ElmProjectConfig exposing (ElmProjectConfig, ElmVersion(..), decoder)
 
 import Json.Decode as Decode exposing (Decoder)
+import String.Interpolate exposing (interpolate)
 
 
 type alias ElmProjectConfig =
@@ -18,4 +19,24 @@ decoder : Decoder ElmProjectConfig
 decoder =
     Decode.map2 ElmProjectConfig
         (Decode.field "source-directories" (Decode.list Decode.string))
-        (Decode.succeed Elm18)
+        (Decode.field "elm-version" Decode.string
+            |> Decode.andThen parseVersion18
+        )
+
+
+parseVersion18 : String -> Decoder ElmVersion
+parseVersion18 versionString =
+    if versionString == elm18VersionString then
+        Decode.succeed Elm18
+
+    else
+        interpolate "Unsupported elm-version value: `{0}`. I only support the exact value {1}."
+            [ versionString
+            , toString elm18VersionString
+            ]
+            |> Decode.fail
+
+
+elm18VersionString : String
+elm18VersionString =
+    "0.18.0 <= v < 0.19.0"
--- tests/ElmProjectConfigTests.elm
+++ tests/ElmProjectConfigTests.elm
@@ -41,4 +41,32 @@ suite =
                             , elmVersion = ElmProjectConfig.Elm18
                             }
                         )
+        , test "Elm 0.18 config with invalid elm-version" <|
+            \() ->
+                """
+{
+    "version": "1.0.0",
+    "summary": "Elm TypeScript",
+    "repository": "https://github.com/dillonkearns/elm-typescript-interop.git",
+    "license": "BSD-3-Clause",
+    "source-directories": [
+        "src",
+        "src/vendor/elm-ast"
+    ],
+    "exposed-modules": [],
+    "dependencies": {
+        "Bogdanp/elm-combine": "3.1.1 <= v < 4.0.0",
+        "dillonkearns/elm-cli-options-parser": "1.0.1 <= v < 2.0.0",
+        "elm-community/list-extra": "7.1.0 <= v < 8.0.0",
+        "elm-community/result-extra": "2.2.0 <= v < 3.0.0",
+        "elm-lang/core": "5.0.0 <= v < 6.0.0",
+        "lukewestby/elm-string-interpolate": "1.0.2 <= v < 2.0.0",
+        "rtfeldman/hex": "1.0.0 <= v < 2.0.0"
+    },
+    "elm-version": "0.18.0"
+}
+              """
+                    |> Decode.decodeString ElmProjectConfig.decoder
+                    |> Expect.equal
+                        (Err "I ran into a `fail` decoder: Unsupported elm-version value: `0.18.0`. I only support the exact value \"0.18.0 <= v < 0.19.0\".")
         ]
