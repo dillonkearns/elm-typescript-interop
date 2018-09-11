5ea68d8da9718d3ce1a2e2838e9c80489528659e
Dillon Kearns <dillon@dillonkearns.com>
2018-09-11 06:55:58 -0700

    Restore elm 19 example.

---
+++ tests-approval/expectSuccess/elm19Simple/src/Main/index.d.ts
@@ -0,0 +1,12 @@
+// WARNING: Do not manually modify this file. It was generated using:
+// https://github.com/dillonkearns/elm-typescript-interop
+// Type definitions for Elm ports
+export as namespace Elm;
+
+export interface App {
+  ports: {};
+}
+
+export namespace Main {
+  export function init(options: { node: HTMLElement | null; flags: null }): App;
+}
