1fe0578cd496db4564dbcba9016f26fbf69b3e4e
Dillon Kearns <dillon@dillonkearns.com>
2018-09-12 11:06:37 -0700

    Add approval test with multiple "main" modules.

---
+++ tests-approval/expectSuccess/multipleMainModules/src/Navbar/index.d.ts
@@ -0,0 +1,28 @@
+// WARNING: Do not manually modify this file. It was generated using:
+// https://github.com/dillonkearns/elm-typescript-interop
+// Type definitions for Elm ports
+
+export namespace Elm {
+  namespace Navbar {
+    export interface App {
+      ports: {
+        loginFromElm: {
+          subscribe(callback: (data: string) => void): void
+        }
+        loginToElm: {
+          send(data: string): void
+        }
+        navbarFromElm: {
+          subscribe(callback: (data: string) => void): void
+        }
+        navbarToElm: {
+          send(data: string): void
+        }
+      };
+    }
+    export function init(options: {
+      node?: HTMLElement | null;
+      flags: null;
+    }): Elm.Navbar.App;
+  }
+}
